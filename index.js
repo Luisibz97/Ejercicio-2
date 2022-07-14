5e056411c39c7c42b03c42366c183744812d8b76
Luisibz97 <luis_rab_20@hotmail.com>
2022-07-13 21:36:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+const lista = [
+    'Luis', 
+    24, 
+    true, 
+    new Date( 1997, 8, 5 ), 
+    {   
+        nombre: 'No tengo, perdón',
+        autor:  'Estoy mas interesado en la programación',
+        fecha:   new Date( 2022, 6 ,13 ),
+        url:     "https://campus.open-bootcamp.com/cursos/15/leccion/1875"
+    }
+]
+
+console.log(lista)
